2e239f864e1c0ab6f630fd45d9d5e2931a2bb72c
Shichao Wang <wangshichao@dbis.nankai.edu.cn>
2021-11-12 10:41:34 +0800

    [Add] RE-GCN training script

---
+++ scripts/train_regcn_ICEWS18.bash
@@ -0,0 +1,11 @@
+#! /usr/bin/bash 
+
+python bin/train_regcn.py \
+    --data-folder "./data/legacy/ICEWS18" \
+    --hist-len 6 \
+    --hidden-size 200 \
+    --num-layers 2 \
+    --kernel-size 3 \
+    --channels 50 \
+    --dropout 0.2 \
+    --lr 1e-3
